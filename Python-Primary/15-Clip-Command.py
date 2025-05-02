73833cae3e55096ba1ce86e3b40ba8493a944661
arka <arkakar1313@gmail.com>
2025-05-02 21:49:28 +0530

    Change for arch-snap

--- Python-Primary/15-Clip-Command.py
+++ Python-Primary/15-Clip-Command.py
@@ -8,13 +8,13 @@ import subprocess       # Run shell script or command in python
 date = datetime.datetime.now()
 formatted_date = date.strftime("%d-%m-%Y")
 
-get_snap_drive = "lsblk | grep 'snap' | head -1 | awk '{print $2}' | sed -e 's/└─//' -e 's/fedora-//' -e 's/--/-/g'"
+get_snap_drive = "lsblk | grep 'snap' | head -1 | awk '{print $2}' | sed -e 's/└─//' -e 's/arch-//' -e 's/--/-/g'"
 snap_drive = subprocess.getoutput(get_snap_drive)
 
 COMMANDS = {
-    'create': f"sudo lvcreate -L 5GB -s -n root-snap-{formatted_date} /dev/mapper/fedora-root",
-    'remove': f"sudo lvremove /dev/fedora/{snap_drive}",
-    'merge': f"sudo lvconvert --merge /dev/fedora/{snap_drive}"
+    'create': f"sudo lvcreate -L 5GB -s -n root-snap-{formatted_date} /dev/mapper/arch-root",
+    'remove': f"sudo lvremove /dev/arch/{snap_drive}",
+    'merge': f"sudo lvconvert --merge /dev/arch/{snap_drive}"
 }
 
 if len(sys.argv) < 2:
